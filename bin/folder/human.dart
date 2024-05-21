359648abb0ae685e769154e4ce6cdcb65b941e5c
Mohamed Adel <75817367+MOHAMMEDadel12@users.noreply.github.com>
2024-05-21 21:14:15 +0300

    Initial commit

---
+++ bin/folder/human.dart
@@ -0,0 +1,51 @@
+class Human {
+  String? name;
+  int? age;
+
+  Human({this.name, this.age});
+
+  void work() {
+    print("WORk");
+  }
+}
+
+mixin Human2 {
+  String? name;
+  int? age;
+
+  void work() {
+    print("WORk");
+  }
+}
+mixin Human3 {}
+
+class Person extends Human with Human2, Human3 {}
+
+// class Developer extends Human {
+//   String? programmingLang;
+//
+//   Developer({this.programmingLang, super.name, super.age});
+// }
+
+// class Doctor implements Human {
+//   @override
+//   int? age;
+//
+//   @override
+//   String? name;
+//
+//   @override
+//   void eat() {
+//     // TODO: implement eat
+//   }
+//
+//   @override
+//   void play() {
+//     // TODO: implement play
+//   }
+//
+//   @override
+//   void work() {
+//     // TODO: implement work
+//   }
+// }
